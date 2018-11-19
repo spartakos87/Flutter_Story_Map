ed382f53a6deb6047947ad44477a3b7d1675d661
spartakos87 <serepasf@yahoo.gr>
2018-11-19 19:45:35 +0200

    Goole maps work fine gps is located me and read and add all markers which exist to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
 
 void main() {
+
   GoogleMapController.init();
   final size = MediaQueryData.fromWindow(ui.window).size;
   final GoogleMapOverlayController controller =
@@ -12,7 +16,22 @@ void main() {
     width: size.width,
     height: size.height,
   );
+
   final mapController = controller.mapController;
+//  Set firebase https://www.youtube.com/watch?v=DqJ_KjFzL9I
+  Firestore.instance
+      .collection('Stories')
+      .snapshots()
+      .listen((data) =>
+      data.documents.forEach((doc) =>
+//Read all the markers from firebase and add them to map
+      AddMarkers(mapController,ConvertCoordinates(doc["lat"], doc["lng"])
+      )));
+
+  mapController.addMarker(MarkerOptions(position: LatLng(0 ,0)));
+  mapController.onMarkerTapped.add((Marker marker){
+                print("Touch marker");
+  });
   final Widget mapWidget = GoogleMapOverlay(controller: controller);
   runApp(
     MaterialApp(
@@ -49,7 +68,11 @@ void main() {
       navigatorObservers: <NavigatorObserver>[controller.overlayController],
     ),
   );
+
+
+
 }
+
 
 class MapsDemo extends StatelessWidget {
   MapsDemo(this.mapWidget, this.controller);
@@ -63,4 +86,15 @@ class MapsDemo extends StatelessWidget {
   }
 }
 
+LatLng ConvertCoordinates(String lat, String lng){
+// Convert strings coordinates to LatLng
+  return LatLng(double.parse(lat), double.parse(lng));
 
+}
+
+
+
+void AddMarkers(GoogleMapController map, LatLng coor){
+
+  map.addMarker(MarkerOptions(position: coor));
+}
