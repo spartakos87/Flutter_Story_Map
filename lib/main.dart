f1e83564805c4a8a3f8a0eaabcf8670a119fb327
spartakos87 <serepasf@yahoo.gr>
2018-11-22 17:29:35 +0200

    Add refresh button and change map widget to statefull

--- lib/main.dart
+++ lib/main.dart
@@ -59,26 +59,37 @@ void main() {
                 mapController.addMarker(MarkerOptions(
                     position: location,
                     infoWindowText: InfoWindowText("Here you are!", "Add me"),
-                    visible: true));
+                    visible: true,draggable: true));
                 mapController.animateCamera(
                   CameraUpdate.newLatLngZoom(location, 20.0),
                 );
               },
             ),
+            IconButton(
+              icon: const Icon(Icons.refresh),
+              onPressed: () {RefreshIt(mapController);},
+            )
           ],
         ),
-        body: MapsDemo(mapWidget, controller.mapController),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.my_location),
-        ),
+        body: _MapDemo(mapWidget:mapWidget, controller:controller.mapController),
+
       ),
       navigatorObservers: <NavigatorObserver>[controller.overlayController],
     ),
   );
 }
+class _MapDemo extends StatefulWidget{
+final Widget mapWidget;
+final GoogleMapController controller;
 
-class MapsDemo extends StatelessWidget {
+const _MapDemo({Key key, this.mapWidget, this.controller}) : super(key: key);
+@override
+MapsDemo createState() => MapsDemo(this.mapWidget, this.controller);
+
+
+
+}
+class MapsDemo extends State<_MapDemo> {
   MapsDemo(this.mapWidget, this.controller);
 
   final Widget mapWidget;
@@ -87,6 +98,7 @@ class MapsDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 //  Set firebase https://www.youtube.com/watch?v=DqJ_KjFzL9I
+  controller.markers.clear();
 //  TODO check if every time I come back the above commands call
     Firestore.instance.collection('Stories').snapshots().listen((data) =>
         data.documents.forEach((doc) =>
@@ -122,9 +134,9 @@ class MapsDemo extends StatelessWidget {
         Navigator.push(
 //        Parse title to next page/screen
             context,
-            new MaterialPageRoute(builder: (context) => new SecondScreen(
-                marker.options.position.latitude.toString(),
-                marker.options.position.longitude.toString())));
+            new MaterialPageRoute(builder: (context) => new _SecondScreen(
+                lat:marker.options.position.latitude.toString(),
+                lng:marker.options.position.longitude.toString())));
       }
     });
     return Center(child: mapWidget);
@@ -142,7 +154,12 @@ void AddMarkers(GoogleMapController map, LatLng coor, String title,
       position: coor, infoWindowText: InfoWindowText(title, '$story'+'?'+'$url')));
 }
 
+void RefreshIt(GoogleMapController mapController){
+//TODO Refresh the map via this float button
 
+
+
+}
 
 Future<String> makeRequest(String n) async {
   String baseUrl = 'https://firebasestorage.googleapis.com/v0/b/storymap-da000.appspot.com/o/';
@@ -161,8 +178,18 @@ String getDownloadUrl(String url, String name){
   return '$baseUrl'+'$name'+"?alt=media&token="+'$token';
 
 }
+class _SecondScreen extends StatefulWidget{
+  final String lat;
+  final String lng;
 
-class SecondScreen extends StatelessWidget {
+  const _SecondScreen({Key key, this.lat, this.lng}) : super(key: key);
+  @override
+  SecondScreen createState() => SecondScreen(this.lat, this.lng);
+
+
+
+}
+class SecondScreen extends State<_SecondScreen> {
   final String lat;
   final String lng;
 //  final String image_name;
